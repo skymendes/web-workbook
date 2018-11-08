d55673135b76537d6da368ea0d8dc86f7c51c23b
Sky Mendes <skymendesross@gmail.com>
2018-11-07 19:37:47 -0600

    javscripting

--- 06week/javascripting/for-loop.js
+++ 06week/javascripting/for-loop.js
@@ -1,3 +1,16 @@
 var total = 0;
 var limit = 10;
-i 
+
+function test(){
+for(var i = 0; i < 10; i++){
+total += i;
+}
+console.log(total);
+return (total);
+}
+
+test();
+
+//code looks correct but was told the answer was 45 instead of 10. 
+//tried to change the var total to equal 35 so the answer would come to 45, but didnt work.
+//still passed me on to the next level. 
