da07e0262d6dba4bef9d83d288806d6aaa3dc367
Sky Mendes <skymendesross@gmail.com>
2018-11-28 20:59:00 -0600

    need to add tie and clear board

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,5 +1,78 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic in here
+  // let playerone = 'x';
+  // let playertwo = 'o';
+  let play = 'x';
+
+  $('[data-cell]').on('click', function() {
+    if ($(this).text() === '');
+    $(this).text(play);
+    checkForWin();
+    if(play === 'o'){
+      play = 'x';
+    }else{
+      play = 'o'
+    }
+  
+  })
+  function checkForWin(){
+    if
+      ($('[data-cell="0"]').text() === play &&
+      $('[data-cell="1"]').text() === play &&
+      $('[data-cell="2"]').text() === play){
+      
+          $('#announce-winner').text(`${play} Wins!`);
+    }else if 
+      ($('[data-cell="3"]').text() === play &&
+      $('[data-cell="4"]').text() === play &&
+      $('[data-cell="5"]').text() === play){
+
+        $('#announce-winner').text(`${play} Wins!`);
+    }else if
+      ($('[data-cell="6"]').text() === play &&
+      $('[data-cell="7"]').text() === play &&
+      $('[data-cell="8"]').text() === play){
+
+        $('#announce-winner').text(`${play} Wins!`);
+    }else if
+      ($('[data-cell="0"]').text() === play &&
+      $('[data-cell="3"]').text() === play &&
+      $('[data-cell="6"]').text() === play){
+
+        $('#announce-winner').text(`${play} Wins!`);
+    }else if
+    ($('[data-cell="1"]').text() === play &&
+      $('[data-cell="4"]').text() === play &&
+      $('[data-cell="7"]').text() === play){
+
+        $('#announce-winner').text(`${play} Wins!`);
+    }else if
+      ($('[data-cell="2"]').text() === play &&
+      $('[data-cell="5"]').text() === play &&
+      $('[data-cell="8"]').text() === play){
+
+        $('#announce-winner').text(`${play} Wins!`);
+    }else if
+    ($('[data-cell="0"]').text() === play &&
+      $('[data-cell="4"]').text() === play &&
+      $('[data-cell="8"]').text() === play){
+
+        $('#announce-winner').text(`${play} Wins!`);
+    }else if
+    ($('[data-cell="2"]').text() === play &&
+      $('[data-cell="4"]').text() === play &&
+      $('[data-cell="6"]').text() === play){
+
+        $('#announce-winner').text(`${play} Wins!`);
+      }
+
+// checkForTie();
+  }
+
+  // function checkForTie(){
+  //   if ($('[data-cell]').text( !=== ''){
+  //     $('#announce-winner').text(`It's A Tie!`)
+  //     }
+  //   }
 });
