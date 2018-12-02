092a751540b2881ea29e56105f767d8bf9dcf019
Sky Mendes <skymendesross@gmail.com>
2018-12-01 19:04:41 -0600

    mostly done...little things need fixing

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -4,20 +4,25 @@ $(document).ready(function() {
   // let playerone = 'x';
   // let playertwo = 'o';
   let play = 'x';
-
+  var count = 0;
   $('[data-cell]').on('click', function() {
     if ($(this).text() === '');
     $(this).text(play);
-    checkForWin();
+    count++;
+    checkForWin(count);
     if(play === 'o'){
       play = 'x';
     }else{
       play = 'o'
     }
     clearBoard();
+    // checkForTie();
   })
-  function checkForWin(){
-
+  function checkForWin(tie){
+    if(tie === 9){
+      $('#announce-winner').text(`It's a tie! Try again`)
+    }
+  
     if
       ($('[data-cell="0"]').text() === play &&
       $('[data-cell="1"]').text() === play &&
@@ -67,20 +72,31 @@ $(document).ready(function() {
 
         $('#announce-winner').text(`${play} Wins!`);
       }
-      var count = 0;
-      for(let i = 0; i < 9; i++){
-        if($('[data-cell]').text()){
-          count++;
-        }
-      }
-      if(count === 9){
-        $('#announce-winner').text(`Try again`);
+      if('[data-cell]' !==0){
+        return false;
       }
   }
+  // function checkForTie(){
+
+    // var tie = (!checkForWin);
+    // for (var i = 0; i < 9; i++){
+    //   if(!$('[data-cell]').text()){
+    //   $('#announce-winner').text(`try again!`);
+    // }
+  
+    // var count = 0;
+    // for(let i = 0; i < 9; i++){
+    //   if(!$('[data-cell]').text()){
+    //     count++;
+    // if ('[data-cell]' === ''){
+    // $('#announce-winner').text(`try again!`);
+    // }
+  // }
+
 function clearBoard(){
   $(document).ready(function(){
     $('#clear').click(function(){
-      $('[data-cell]').empty();
+      location.reload();
     });
   });
 }
