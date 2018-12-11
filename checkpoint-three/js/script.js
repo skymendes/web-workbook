6ccd69043532e82cd4d24f36124619e55129e706
Sky Mendes <skymendesross@gmail.com>
2018-12-10 20:06:11 -0600

    colors work, win does not

--- checkpoint-three/js/script.js
+++ checkpoint-three/js/script.js
@@ -1,1 +1,30 @@
 'use strict'
+//clicking function for moles
+//if mole is orange
+//click changes to black and add point
+//change board value to show mole has been whacked
+
+//check for win
+//check all values
+//if all whacked - announce win
+
+//create timer for mole to switch
+
+$(document).ready(function() {
+    var gameboard = ".board"
+    var moles = ".mole"
+    var score = ".score"
+//Setting mole to change color when clicked
+//Switching mole back to black after 2s
+$(moles).click(function () {
+    var v = $(this).text();
+    $(this).css("background-color", 'red');
+    setTimeout(function(){
+        $(moles).css("background-color", 'black');
+    },8000);
+});
+
+if($("#mole1").css("background-color") === "red"){
+    console.log(($("winner").text(`You Win!`)));
+}
+});
