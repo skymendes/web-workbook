278a742e423a9cf7ee5fe4669f24ce01a1d46673
Sky Mendes <skymendesross@gmail.com>
2018-12-12 19:08:03 -0600

    final

--- checkpoint-three/js/script.js
+++ checkpoint-three/js/script.js
@@ -1,32 +1,15 @@
 'use strict'
-//clicking function for moles
-//if mole is orange
-//click changes to black and add point
-//change board value to show mole has been whacked
-
-//check for win
-//check all values
-//if all whacked - announce win
-
-//create timer for mole to switch
 
 $(document).ready(function() {
     var gameboard = ".board"
     var moles = ".mole"
-    //var array = ".wholeBoard"
-    //var newArray = $.makeArray(array)
-//console.log(newArray);
-    // for(i=0; i<moles.length; i++){
 
-    // }
-    
-    // var score = ".score"
     clearBoard();
 //Setting mole to change color when clicked
 //Switching mole back to black after 2s
     $(moles).each(function(index){
         $("#mole" + index).click(function () {
-            $("#mole" + index).css("background-color", "rgb(255,20,147)");
+            $("#mole" + index).css("background-color", 'deeppink');
             setTimeout(function() {
                 $("#mole" + index).css("background-color", 'black');
             },getRandomTimeout());   
@@ -34,22 +17,21 @@ $(document).ready(function() {
         });  
     }); 
     function checkForWin() {
-        if($("#mole0").css("background-color") === ("rgb(255,20,147)") &&
-            $("#mole1").css("background-color") === ("rgb(255,20,147)") &&
-            $("#mole2").css("background-color") === ("rgb(255,20,147)") &&
-            $("#mole3").css("background-color") === ("rgb(255,20,147)") &&
-            $("#mole4").css("background-color") === ("rgb(255,20,147)") && 
-            $("#mole5").css("background-color") === ("rgb(255,20,147)") &&
-            $("#mole6").css("background-color") === ("rgb(255,20,147)") &&
-            $("#mole7").css("background-color") === ("rgb(255,20,147)") &&
-            $("#mole8").css("background-color") === ("rgb(255,20,147)")){
+        if($("#mole0").css("background-color") === ("rgb(255, 20, 147)") &&
+            $("#mole1").css("background-color") === ("rgb(255, 20, 147)") &&
+            $("#mole2").css("background-color") === ("rgb(255, 20, 147)") &&
+            $("#mole3").css("background-color") === ("rgb(255, 20, 147)") &&
+            $("#mole4").css("background-color") === ("rgb(255, 20, 147)") && 
+            $("#mole5").css("background-color") === ("rgb(255, 20, 147)") &&
+            $("#mole6").css("background-color") === ("rgb(255, 20, 147)") &&
+            $("#mole7").css("background-color") === ("rgb(255, 20, 147)") &&
+            $("#mole8").css("background-color") === ("rgb(255, 20, 147)")){
         $("#winner").text('You Win!');
         }
     }
     function getRandomTimeout() {
         return Math.floor((Math.random()*5500) +2500);
     }
-    clearTimeout(checkForWin);
     function clearBoard(){
         $(document).ready(function(){
             $('#reset').click(function(){
