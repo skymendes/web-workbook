66429f94ef9d598f3c70fe95e3f34a31538ff05a
Sky Mendes <skymendesross@gmail.com>
2019-11-07 15:37:05 -0600

    making changes

--- 06week/rps/js/script.js
+++ 06week/rps/js/script.js
@@ -1,18 +1,19 @@
-var p1;
-var p2;
+// var p1;
+// var p2;
 
-p1 = prompt("Rock, Paper, Scissors");
-p2 = prompt("Rock, Paper, Scissors");
+// p1 = prompt("Rock, Paper, Scissors");
+// p2 = prompt("Rock, Paper, Scissors");
 
-function rps(p1, p2){
-    if(p1 === p2){
-        document.write("It's a tie!");
-    }
-    else if((p1 === "rock" && p2 === "scissors")||(p1 === "scissors" && p2 === "paper")||(p1 === "paper" && p2 === "rock")){
-        document.write("Player 1 wins!!");
-    }
-    else if((p2 === "rock" && p1 === "scissors")||(p2 === "scissors" && p1 === "paper")||(p2 === "paper" && p1 === "rock")){
-        document.write("Player 2 wins!!");
-    }
-}
-rps(p1, p2);
+// function rps(p1, p2){
+//     if(p1 === p2){
+//         document.write("It's a tie!");
+//     }
+//     else if((p1 === "rock" && p2 === "scissors")||(p1 === "scissors" && p2 === "paper")||(p1 === "paper" && p2 === "rock")){
+//         document.write("Player 1 wins!!");
+//     }
+//     else if((p2 === "rock" && p1 === "scissors")||(p2 === "scissors" && p1 === "paper")||(p2 === "paper" && p1 === "rock")){
+//         document.write("Player 2 wins!!");
+//     }
+// }
+// rps(p1, p2);
+
